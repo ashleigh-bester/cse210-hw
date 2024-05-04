6c5909671dc01c2590ac09eb4d1726a1e93cc1fc
Ashleigh Bester <ashleighbester8@gmail.com>
2024-05-04 15:18:42 +0200

    design activity: class stubs

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Entry
+{
+    public string _date;
+    public string _prompText;
+    public string _entryText;
+
+    public void Display()
+    {
+        
+    }
+}
