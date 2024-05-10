cdd27b2e628f451f09a150eebca0eb8a67df66ab
Ashleigh Bester <ashleighbester8@gmail.com>
2024-05-11 00:32:42 +0200

    develop03 stubs

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chpater, int verse)
+    {
+
+    }
+
+    public Reference(string book, int chpater, int startVerse, int endVerse)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
