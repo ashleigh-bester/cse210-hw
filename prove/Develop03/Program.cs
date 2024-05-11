2c2cde8b2e15bfeac7b9171db5967a922c9ec462
Ashleigh Bester <ashleighbester8@gmail.com>
2024-05-11 23:52:23 +0200

    develop03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,6 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("");
         
         Reference reference = new Reference("Alma", 32, 21, 22);
         Console.WriteLine(reference.GetDisplayText());
