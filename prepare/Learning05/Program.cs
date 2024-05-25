011c9c701d5b12c20f47f1f4ce27b5d5557dd12c
Ashleigh Bester <ashleighbester8@gmail.com>
2024-05-25 21:46:21 +0200

    Learning05 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 4);
+        //Console.WriteLine(square.GetColor());
+        //Console.WriteLine(square.GetArea());
+        //Console.WriteLine();
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("pink", 3, 4);
+        //Console.WriteLine(rectangle.GetColor());
+        //Console.WriteLine(rectangle.GetArea());
+        //Console.WriteLine();
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("green", 2);
+        //Console.WriteLine(circle.GetColor());
+        //Console.WriteLine(circle.GetArea());
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
